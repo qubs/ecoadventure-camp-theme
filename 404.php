58b69352c0aefd32937c9414222bbc4788728448
David Lougheed <david.lougheed@gmail.com>
2017-06-28 14:53:23 -0400

    Add 404 error page.

---
+++ 404.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+
+<h2>404: Page Not Found</h2>
+<p>No page was found at this location. Try searching for what you're looking for:</p>
+
+<?php
+get_search_form();
+get_footer();
